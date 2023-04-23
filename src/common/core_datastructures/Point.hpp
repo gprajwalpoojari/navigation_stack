3bd0434a4231e97ff21c2915f445e9de6c084a64
Prajwal Poojari <gprajwalpoojari@gmail.com>
2023-04-23 11:34:06 -0400

    Added commonly used data structures

---
+++ src/common/core_datastructures/Point.hpp
@@ -0,0 +1,10 @@
+#ifndef POINT_HPP
+#define POINT_HPP
+
+struct Point {
+    double x;       // x position
+    double y;       // y position
+
+};
+
+#endif  /*POINT_HPP*/
---
+++ src/common/core_datastructures/Posture.hpp
@@ -0,0 +1,11 @@
+#ifndef POSTURE_HPP
+#define POSTURE_HPP
+
+struct Posture {
+    double x;       // x position
+    double y;       // y position
+    double theta;   // orientation
+    double kappa;   // curvature
+};
+
+#endif  /*POSTURE_HPP*/
