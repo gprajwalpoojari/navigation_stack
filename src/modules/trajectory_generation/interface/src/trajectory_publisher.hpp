7df1301ca459cd2d5836d659738d57b167cf2614
Prajwal <gprajwalpoojari@gmail.com>
2023-06-04 15:28:14 -0400

    Fixed conflicts

--- src/modules/trajectory_generation/interface/src/trajectory_publisher.hpp
+++ src/modules/trajectory_generation/interface/src/trajectory_publisher.hpp
@@ -2,39 +2,9 @@
 #define TRAJECTORY_PUBLISHER_HPP
 
 #include <rclcpp/rclcpp.hpp>
-#include <common_ros2/msg/splines.hpp>
+#include <common_ros2/msg/Trajectory.hpp>
 #include <core_datastructures/posture.hpp>
-#include <spline_generation/i_spline_generator.hpp>
-#include <graph_generation/graph_generator.hpp>
-#include <visualization_msgs/msg/marker.hpp>
-#include <string>
-#include <iostream>
-
-
-/**
- * @brief Class to publish trajectory in Rviz
- * 
- */
-class RvizPublisher 
-{
-public:
-  RvizPublisher(rclcpp::Node* node,
-                  std::string frame_id,
-                  float point_scale,
-                  float line_scale);
-
-  void publish();
-
-  void add_point(const geometry_msgs::msg::Point& p);
-
-  void add_line(const geometry_msgs::msg::Point& p);
-
-private:
-  rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr visualize_;
-  visualization_msgs::msg::Marker points;
-  visualization_msgs::msg::Marker line_strip;
-};
-
+#include <trajectory_generation/i_trajectory_generator.hpp>
 
 /** 
  * @brief Trajectory Publisher class for publishing trajectories
@@ -59,12 +29,11 @@ class TrajectoryPublisher : public rclcpp::Node{
     
     rclcpp::TimerBase::SharedPtr timer_;
     // TODO(PP) - Remove graph generator and replace it with a trajectory generator.
-    std::shared_ptr<trajectory_generation::graph_generation::GraphGenerator> graph_generator_;
+    std::shared_ptr<trajectory_generation::trajectory_generation::ITrajectoryGenerator> trajectory_generator_;
     // TODO(PP) - Convert "Splines" Publisher into trajectory publisher.
     // TODO(PP) - Remove "Splines.msg" file from common as it is required only for debugging
-    rclcpp::Publisher<common_ros2::msg::Splines>::SharedPtr publisher_;
-    
-    core_datastructures::Posture start_;
+    rclcpp::Publisher<common_ros2::msg::Trajectory>::SharedPtr publisher_;
+    core_datastructures::DynamicPosture start_;
     core_datastructures::Posture goal_;
     bool callback_executed = false;
     RvizPublisher rviz;
