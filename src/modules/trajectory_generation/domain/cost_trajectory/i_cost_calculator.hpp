9cf3aedec1100ec4b6ca908ec345315a291958a9
Prajwal <gprajwalpoojari@gmail.com>
2023-06-21 14:07:40 -0400

    Updated documentation

--- src/modules/trajectory_generation/domain/cost_trajectory/i_cost_calculator.hpp
+++ src/modules/trajectory_generation/domain/cost_trajectory/i_cost_calculator.hpp
@@ -4,5 +4,10 @@
 
 class ICostCalculator {
     public:
+    /**
+     * @brief Get the overall cost for an object
+     * 
+     * @return double 
+     */
     virtual double get_cost() const = 0;
 };
